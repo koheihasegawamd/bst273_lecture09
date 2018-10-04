30dcf7655280d11c304738b178c235cfa49dcf46
Kohei <koheihasegawamd@gmail.com>
2018-10-04 12:28:17 -0400

    open and read lines of file

--- word_count.py
+++ word_count.py
@@ -3,7 +3,6 @@
 import argparse
 
 parser = argparse.ArgumentParser( description="" )
-
 parser.add_argument(
 #1. Get our input file as a commandline argument (probably using argparse)
 	"data_file",
@@ -21,5 +20,17 @@ args = parser.parse_args( )
 # our code for analyzing the data
 #-------------------------------------------------------------------------------
 
-print(args)
-print(args.data_file)
+# Sanity check for args
+print("argss=", args)
+
+#2. Open the file
+fh = open( args.data_file )
+print("the file handle is", fh)
+#3. Initialize variables for lines, words, and bytes
+lines = 0
+words = 0
+chars = 0
+#4. Read the file line by line
+for line in fh:
+	print(line)
+	lines += 1
