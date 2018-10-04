8706befc2b4db1e3b5e949f0caffd48f92c27f49
Kohei <koheihasegawamd@gmail.com>
2018-10-04 12:36:21 -0400

    added line numbers

--- word_count.py
+++ word_count.py
@@ -2,7 +2,9 @@
 
 import argparse
 
-parser = argparse.ArgumentParser( description="" )
+parser = argparse.ArgumentParser( description =
+	"returns the number of lines, words, and bytes (characters) of a file. INPUT: a file path. OUTPUTs: count of lines, count of words, count of bytes" )
+
 parser.add_argument(
 #1. Get our input file as a commandline argument (probably using argparse)
 	"data_file",
@@ -26,11 +28,13 @@ print("argss=", args)
 #2. Open the file
 fh = open( args.data_file )
 print("the file handle is", fh)
-#3. Initialize variables for lines, words, and bytes
+#3. Initialize output variables for lines, words, and bytes
 lines = 0
 words = 0
 chars = 0
 #4. Read the file line by line
 for line in fh:
-	print(line)
-	lines += 1
+	#print(line)  # Sanity check
+	lines += 1   # for each line, add 1 (ie, count) to the lines variable
+	words += len( line.split() )
+print("The number of lines is:", lines)
